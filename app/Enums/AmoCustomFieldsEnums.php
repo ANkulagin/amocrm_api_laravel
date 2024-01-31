3df6731353868d0a9cf6c83d87ae01ae627b874a
ANkulagin <ankulskull@gmail.com>
2024-01-31 16:29:14 +0300

    правка enum

--- app/Enums/AmoCustomFieldsEnums.php
+++ app/Enums/AmoCustomFieldsEnums.php
@@ -9,21 +9,17 @@ enum AmoCustomFieldsEnums
     case PHONE;
     case AGE_FIELDS_ID;
     case GENDER_FIELDS_ID;
-    public function getAmoCustomFields(AmoCustomFieldsEnums $amo)
+
+    public function getValue(): mixed
     {
-        switch ($amo) {
-            case self::EMAIL:
-                return 'EMAIL';
-            case self::PHONE:
-                return 'PHONE';
-            case self::AGE_FIELDS_ID:
-                return 634311;
-            case self::GENDER_FIELDS_ID:
-                return 647239;
-        }
+        return match ($this) {
+            self::EMAIL => 'EMAIL',
+            self::PHONE => 'PHONE',
+            self::AGE_FIELDS_ID => 634311,
+            self::GENDER_FIELDS_ID => 647239,
+        };
     }
 }
-
 
 
 
--- app/Http/Controllers/AmoController.php
+++ app/Http/Controllers/AmoController.php
@@ -181,7 +181,7 @@ class AmoController extends Controller
             $customFields = new CustomFieldsValuesCollection();
             $contact->setCustomFieldsValues($customFields);
             //PHONE
-            $phoneField = (new MultitextCustomFieldValuesModel())->setFieldCode(AmoCustomFieldsEnums::getAmoCustomFields(AmoCustomFieldsEnums::PHONE));
+            $phoneField = (new MultitextCustomFieldValuesModel())->setFieldCode(AmoCustomFieldsEnums::PHONE->getValue());
             $customFields->add($phoneField);
             // Установка значения поля
             $phoneField->setValues(
@@ -193,7 +193,7 @@ class AmoController extends Controller
                     )
             );
             //EMAIL
-            $emailField = (new TextCustomFieldValuesModel())->setFieldCode(AmoCustomFieldsEnums::getAmoCustomFields(AmoCustomFieldsEnums::EMAIL));
+            $emailField = (new TextCustomFieldValuesModel())->setFieldCode(AmoCustomFieldsEnums::EMAIL->getValue());
             $customFields->add($emailField);
             // Установка значения поля
             $emailField->setValues(
@@ -205,7 +205,7 @@ class AmoController extends Controller
             );
             //AGE
             // Проверка наличия поля 'Возвраст' по ID
-            $ageField = (new TextCustomFieldValuesModel())->setFieldId(AmoCustomFieldsEnums::getAmoCustomFields(AmoCustomFieldsEnums::AGE_FIELDS_ID));
+            $ageField = (new TextCustomFieldValuesModel())->setFieldId(AmoCustomFieldsEnums::AGE_FIELDS_ID->getValue());
             $customFields->add($ageField);
             // Установка значения поля
             $ageField->setValues(
@@ -216,7 +216,7 @@ class AmoController extends Controller
                     )
             );
             //GENDER
-            $genderField = (new TextCustomFieldValuesModel())->setFieldId(AmoCustomFieldsEnums::getAmoCustomFields(AmoCustomFieldsEnums::GENDER_FIELDS_ID));
+            $genderField = (new TextCustomFieldValuesModel())->setFieldId(AmoCustomFieldsEnums::GENDER_FIELDS_ID->getValue());
             $customFields->add($genderField);
             // Установка значения поля
             $genderField->setValues(
